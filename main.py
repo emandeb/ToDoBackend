90344915110930e29f34c7ce20b44ffc9c7770a0
Elías Mandeb <maxernestot@gmail.com>
2023-06-28 23:03:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import json
 
+app = FastAPI()
