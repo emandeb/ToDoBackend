886a8fd170541b84b4e84f020a6e4f12eb0558f7
emandeb <maxernestot@gmail.com>
2023-06-30 01:05:45 -0300

    Co-authored-by: Kevin Kener <albakore@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse, FileResponse 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
 
+
 app = FastAPI()
 
-@app.get('/')
-def root():
-  return 'Hola mundo'
+# templates = Jinja2Templates(directory='templates')
+
+
+# @app.get('/')
+# def root(request : Request):
+#   return templates.TemplateResponse('index.html',{'request': request})
+
+app.mount('/',StaticFiles(directory="templates"),name="static")
